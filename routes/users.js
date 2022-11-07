66cce0e7f81c0b7e17da8346ab37846847518b26
zhanhao@oritech.com <zhanhao@oritech.com>
2022-11-07 22:59:11 +0800

    save

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const router = require("koa-router")()
+const { register } = require("../controller/user")
+const { ErrorModel } = require("../model/resModel")
+
+router.prefix("/users")
+
+router.get("/", function (ctx, next) {
+  ctx.body = "this is a users response!"
+})
+
+router.get("/bar", function (ctx, next) {
+  ctx.body = "this is a users/bar response"
+})
+
+router.post("/new-user", async function (ctx, next) {
+  const { username } = ctx.request.body
+  const data = await register(username)
+  if (data) {
+    ctx.body = new SuccessModel()
+    return
+  }
+  ctx.body = new ErrorModel("登录失败")
+})
+
+module.exports = router
